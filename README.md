ef2d911d51254339997685f65b407fd074f4e9ec
Renato Soares <renatosoarespro@hotmail.com>
2017-06-18 13:01:27 -0300

    Project configuration, skeleton and migrations with phinx

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# modern-bakery
+# Modern Bakery
 Final work for the discipline of rich interfaces
+
+
+#### :mortar_board:
+##### Tecnologia em Análise e Desenvolvimento de Sistemas
+###### Instituto Federal de Educação, Ciência e Tecnologia 
+
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+defined("DBDRIVER")or define('DBDRIVER','postgres');
+defined("DBHOST")or define('DBHOST','127.0.0.1');
+defined("DBNAME")or define('DBNAME','modern-bakery');
+defined("DBUSER")or define('DBUSER','practicedev');
+defined("DBPASS")or define('DBPASS','masterkey');
